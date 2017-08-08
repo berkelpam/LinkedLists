9b077f2d0b9761ab29e7c1b8d1502dfdeaee62ca
Paul Java <pauljava@Pauls-MacBook-Pro.local>
2017-08-08 16:30:00 +0200

    finished demo 1

--- src/com/company/Demo.java
+++ src/com/company/Demo.java
@@ -1,10 +1,7 @@
 package com.company;
 
-import com.sun.org.apache.xerces.internal.xs.StringList;
-
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.ListIterator;
 
 /**
@@ -13,7 +10,7 @@ import java.util.ListIterator;
 public class Demo {
 
     public static void main(String[] args) {
-        LinkedList<String> placesToVisit=new LinkedList<String>();
+        LinkedList<String> placesToVisit = new LinkedList<String>();
 
         placesToVisit.add("Sidney");
         placesToVisit.add("Meldourne");
@@ -22,7 +19,7 @@ public class Demo {
 
         printList(placesToVisit);
 
-        placesToVisit.add(1,"Alice Springs");
+        placesToVisit.add(1, "Alice Springs");
 
         printList(placesToVisit);
         placesToVisit.remove(3);
@@ -31,40 +28,39 @@ public class Demo {
 
     }
 
-    private static void printList(LinkedList<String> linkedList){
-        Iterator<String> i=linkedList.iterator();
-        while (i.hasNext()){
+    private static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+        while (i.hasNext()) {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("======================");
     }
 
-    private static boolean addInOrder (LinkedList<String> linkedList,String newCity){
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
         ListIterator<String> StringListIterator = linkedList.listIterator();
 
-        while(StringListIterator.hasNext()){
+        while (StringListIterator.hasNext()) {
             int comparison = StringListIterator.next().compareTo(newCity);
 
-            if (comparison==0){
+            if (comparison == 0) {
                 //do not add
                 System.out.println("City is already added as destionation");
                 return false;
-            }
-            else if (comparison>0){
+            } else if (comparison > 0) {
                 //new city should before this one
                 StringListIterator.previous();
                 StringListIterator.add(newCity);
                 return true;
-            }
-            else if(comparison<0){
+            } else if (comparison < 0) {
                 // move on to the next city
             }
 
-            }
-        StringListIterator.add(newCity);
         }
-
+        StringListIterator.add(newCity);
+        return true;
     }
 
-    }
+}
+
+
 
